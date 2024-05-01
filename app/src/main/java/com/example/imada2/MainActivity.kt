78a242e4f639d8bcdeff38acd42b617691470a73
praisecoding <ngalimapraise@gmail.com>
2024-05-01 19:56:01 +0200

    IMAP PROJECT2 updated

--- app/src/main/java/com/example/imada2/MainActivity.kt
+++ app/src/main/java/com/example/imada2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.imada2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,11 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-      val startButton = findViewById<Button>(R.id.START)
+       // once button is pressed it should lead to the next page.
+        val startButton = findViewById<Button>(R.id.START)
 
+        // this gives the user the freedom to give the pet a name
+        val name = findViewById<EditText>(R.id.NAME)
+
+        val userName = name.text.toString()
         startButton.setOnClickListener {
             val Intent = Intent( this, SecondPage::class.java)
+            Intent.putExtra("name",name.text.toString())
             startActivity(Intent)
         }
+
     }
 }
--- app/src/main/java/com/example/imada2/SecondPage.kt
+++ app/src/main/java/com/example/imada2/SecondPage.kt
@@ -1,7 +1,9 @@
 package com.example.imada2
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -15,33 +17,123 @@ class SecondPage : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_second_page)
 
-        val feedButton = findViewById<Button>(R.id.FEED)
-        val cleanButton = findViewById<Button>(R.id.CLEAN)
-        val playButton = findViewById<Button>(R.id.PLAY)
+        var feedbutton = findViewById<Button>(R.id.FEED)
+        var cleanbutton = findViewById<Button>(R.id.CLEAN)
+        var playButton = findViewById<Button>(R.id.PLAY)
+        var home = findViewById<Button>(R.id.HOME)
+        var comment = findViewById<TextView>(R.id.COMMENT)
         var hungerView = findViewById<TextView>(R.id.HUNGER)
         var cleanView = findViewById<TextView>(R.id.CLEANTEXT)
         var happyView = findViewById<TextView>(R.id.HAPPY)
         var image = findViewById<ImageView>(R.id.IMAGE)
+        // this decleration gets the inputed name from the first page and uses the data on this page.
+        var Intent = intent
+        var name= Intent.getStringExtra("name")
 
+        // these are the numbers the dog is going to start on when app is launched
+        var food = 4
+        var funn = 4
+        var cleaness = 4
 
-        feedButton.setOnClickListener {
-            hungerView.text= "0"
-            cleanView.text="3"
-            happyView.text="10"
-            image.setImageDrawable(getDrawable(R.drawable.dog_eating))
+        // function for feed, this is what will happen when the feed button is pressed
+        // the less the hunger is the more hungry the dog will be
+        // while the dog eats it also gets dirty, that's why cleaness decreases
+        fun food() {
+            if (food >= 0 && food < 10 && cleaness > 0) {
+                comment.text = " $name is eating."
+                food += 1
+                cleaness -= 1
+                hungerView.text = "$food"
+                cleanView.text = "$cleaness"
+                happyView.text = "$funn"
+                image.setImageDrawable(getDrawable(R.drawable.dog_eating))
+            } else if (food == 10) {
+                comment.text = "$name is full."
+                hungerView.text = "$food"
+                cleanView.text = "$cleaness"
+                happyView.text = "$funn"
+                image.setImageDrawable(getDrawable(R.drawable.dog_full))
+            } else {
+                comment.text = "$name needs a bath."
+                hungerView.text = "$food"
+                cleanView.text = "$cleaness"
+                happyView.text = "$funn"
+                image.setImageDrawable(getDrawable(R.drawable.dirty_dog))
+            }
+
         }
-        cleanButton.setOnClickListener {
-            hungerView.text= "6"
-            cleanView.text="10"
-            happyView.text="8"
-            image.setImageDrawable(getDrawable(R.drawable.dog_bathing))
+        // the higher the clean value goes up the more clean the dog becomes
+        // the cleaner it gets the more it wants to play.
+        fun clean() {
+            if (cleaness >= 0 && cleaness < 10 && funn > 0) {
+                comment.text = "$name is bathing."
+                cleaness += 1
+                funn -= 1
+                hungerView.text = "$food"
+                cleanView.text = "$cleaness"
+                happyView.text = "$funn"
+                image.setImageDrawable(getDrawable(R.drawable.dog_bathing))
+            } else if (cleaness == 10) {
+                comment.text = "$name is completley clean."
+                hungerView.text = "$food"
+                cleanView.text = "$cleaness"
+                happyView.text = "$funn"
+                image.setImageDrawable(getDrawable(R.drawable.clean_dog))
+            } else {
+                comment.text = "$name wants to play."
+                hungerView.text = "$food"
+                cleanView.text = "$cleaness"
+                happyView.text = "$funn"
+                image.setImageDrawable(getDrawable(R.drawable.dog_wants_to_play_))
+            }
+        }
+        //the higher the happy number increases that means the dog is having a lot of fun
+        // the more energy it uses the more hungry it gets thus hunger decreases
+        fun plaay() {
+            if (funn >= 0 && funn < 10 && food > 0) {
+                comment.text = "$name is playing."
+                funn += 1
+                food -= 1
+                hungerView.text = "$food"
+                cleanView.text = "$cleaness"
+                happyView.text = "$funn"
+                image.setImageDrawable(getDrawable(R.drawable.dog_playing))
+            } else if (funn == 10) {
+                comment.text = "$name is tired."
+                hungerView.text = "$food"
+                cleanView.text = "$cleaness"
+                happyView.text = "$funn"
+                image.setImageDrawable(getDrawable(R.drawable.tired_dog))
+
+            } else {
+                comment.text = " Feed $name."
+                hungerView.text = "$food"
+                cleanView.text = "$cleaness"
+                happyView.text = "$funn"
+                image.setImageDrawable(getDrawable(R.drawable.hungry_dog))
+            }
+        }
+        feedbutton.setOnClickListener {
+            food()
+        }
+        cleanbutton.setOnClickListener {
+            clean()
         }
         playButton.setOnClickListener {
-            hungerView.text= "9"
-            cleanView.text="3"
-            happyView.text="10"
-            image.setImageDrawable(getDrawable(R.drawable.dog_playing))
+            plaay()
         }
+
+        // this helps user go back to the start this thus means dog will be reset.
+        home.setOnClickListener {
+            val Intent1 = Intent( this, MainActivity::class.java)
+            startActivity(Intent1)
         }
+
+
     }
+}
+
+
+
+
 
